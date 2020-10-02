9ea87d8d262f7031d45a0bc10c1d4cccbef5667a
Brennan <71230741+baj54492@users.noreply.github.com>
2020-10-02 12:18:59 -0500

    Initial commit

---
+++ forms/membership/membership.js
@@ -0,0 +1,13 @@
+/*
+let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"];
+let firstName = "";
+btnSubmitUser.onclick=function(){
+  firstName = inptFirstName.value
+  if(members.includes(firstName))
+    lblMembershipForm.textContent = 'You are a member!'
+  else
+    lblMembershipForm.textContent = 'You are not a member, let me add you!'
+    members.push(firstName);
+    console.log(members);
+}
+*/
